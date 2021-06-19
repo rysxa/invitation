450f50b59b645754cd4fd93f396b06cc8f3c7d94
Indry Sefviana <indrysfa@gmail.com>
2021-06-20 01:10:51 +0700

    edit dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,35 +4,23 @@
     <!-- Begin Page Content -->
     <div class="container-fluid">
 
-      <!-- Page Heading -->
-      {{-- <h1 class="h3 mb-4 text-gray-800">Blank Page</h1> --}}
+        <!-- Page Heading -->
+        {{-- <h1 class="h3 mb-4 text-gray-800">Blank Page</h1> --}}
 
-      <!-- Content Row -->
-      <div class="row">
+        <!-- Content Row -->
+        <div class="row">
 
-        <!-- Border Left Utilities -->
-        <div class="col-lg-6">
+            <!-- Border Left Utilities -->
+            <div class="col-lg-12">
+                <marquee scrolldelay="50" title="Welcome">
+                    <h3>Welcome to Wedding Invitation from <a href="http://sefviana.com/">sefviana.com</a></h3>
+                </marquee>
+                <img class="img-fluid" src="{{ asset('images/wedding-invitation.jpg') }}" alt="wedding-invitation">
 
-            <div class="card mb-4 py-3 border-left-primary">
-                <div class="card-body">
-                    <a href="#">Title</a>
-                </div>
-            </div>
-            <div class="card mb-4 py-3 border-left-primary">
-                <div class="card-body">
-                    <a href="#">Hello</a>
-                </div>
-            </div>
-            <div class="card mb-4 py-3 border-left-primary">
-                <div class="card-body">
-                    <a href="#">Event</a>
-                </div>
             </div>
 
         </div>
 
-      </div>
-
-  </div>
-  <!-- /.container-fluid -->
+    </div>
+    <!-- /.container-fluid -->
 @endsection
